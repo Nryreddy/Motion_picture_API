0b8879d35f53a9e26eb0ce8612a511f301ad01a9
Nry <nitheshm.19.becs@acharya.ac.in>
2023-08-13 17:50:47 +0530

    Initial commit

---
+++ src/test/java/dev/nry/motion_picture/MotionPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nry.motion_picture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotionPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
