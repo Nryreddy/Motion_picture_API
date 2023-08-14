3b53f6ec3c67425097a630914b2e3ff36b36d4e5
Nry <nitheshm.19.becs@acharya.ac.in>
2023-08-14 19:40:43 +0530

    project_ b struct

---
+++ src/main/java/dev/nry/motion_picture/MovieController.java
@@ -0,0 +1,4 @@
+package dev.nry.motion_picture;
+
+public class MovieController {
+}
---
+++ src/main/java/dev/nry/motion_picture/Review.java
@@ -0,0 +1,21 @@
+package dev.nry.motion_picture;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.core.SpringVersion;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    private ObjectId id;
+
+    private String body;
+}
